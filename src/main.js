e4d553896a7445799a69b364e334f27006bc5317
Anderson V Santos <andersonvs.ju@gmail.com>
2024-10-14 20:26:33 -0300

    Finished Exercise

---
+++ src/main.js
@@ -0,0 +1,9 @@
+"use strict";
+function multiplicar(n1, n2) {
+    return n1 * n2;
+}
+function saldar(nome) {
+    return `Olá ${nome}`;
+}
+console.log(multiplicar(2, 3));
+console.log(saldar('Anderson'));
---
+++ src/main.ts
@@ -0,0 +1,10 @@
+function multiplicar(n1: number, n2: number): number {
+    return n1 *n2;
+}
+
+function saldar(nome: string): string {
+    return `Olá ${nome}`
+}
+
+console.log(multiplicar(2, 3));
+console.log(saldar('Anderson'));
